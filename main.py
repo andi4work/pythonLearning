b29dbf43db8eb35bbbaed3d96f5737080f5a970a
Andi4work <andi4work@gmail.com>
2023-01-02 16:00:40 +0800

    Added Dictionary function

--- main.py
+++ main.py
@@ -6,12 +6,27 @@
 def print_name(name, student_id):
     print('Student name is {}\nStudent id is {}'.format(name, student_id))
     print('\n')
-    print('Student name is {one}\nStudent id is {two}'.format(one = name, two =student_id))
+    print('Student name is {one}\nStudent id is {two}'.format(one=name, two=student_id))
+    names()
+
+
+def names():
+    names = ['Arjun', 'Avinash', 'Balaji', 'Bala Krishna', 'Bhanu Teja', 'Bharath', 'Chandramohan Reddy',
+             [401, 402, 403, 404, 405, 406, 407]]
+    print(names[6:7])
+    print(names[7][6])
+
+
+def dicx():
+    d = {'Arjun': 401, 'Avinash': 402, 'Balaji': 403, 'BhanuTeja': 404, 'BalaKrishna': 405, 'Bharath': 406,
+         'Chandramohan': {'details': [407, 2010, 8861163436]}}
+    print("Year of joining: ")
+    print(d['Chandramohan']['details'][1])
 
 
 # Press the green button in the gutter to run the script.
 
-if __name__ == "__main__":
-    print_name('Reddy', '111221007')
+if __name__ == '__main__':
+    dicx()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
