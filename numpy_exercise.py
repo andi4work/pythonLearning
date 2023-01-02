6796c9fae32a86c7dd69b20a07c8cf4ff68514e3
andi4work <andi4work@gmail.com>
2023-01-02 16:43:18 +0800

    Added numpy_exercise file

--- numpy_exercise.py
+++ numpy_exercise.py
@@ -1,5 +1,16 @@
-import numpy_exercise as np
+import numpy as np
 
-arr = np.arange(0,11)
 
-print(arr)
+def play_with_numers():
+    arr = np.arange(1, 61)
+    i = 0
+    for i in arr:
+        if i <= 9:
+            print('10HN1A040' +str(i))
+        else:
+            print('10HN1A04' + str(i))
+
+
+
+if __name__ == '__main__':
+    play_with_numers()
