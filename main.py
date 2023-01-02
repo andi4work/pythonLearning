3b64d32ff3924b60d6acbfbd54e611ef4b8f6b17
Andi4work <andi4work@gmail.com>
2023-01-02 12:58:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_name(name, student_id):
+    print('Student name is {}\nStudent id is {}'.format(name, student_id))
+    print('\n')
+    print('Student name is {one}\nStudent id is {two}'.format(one = name, two =student_id))
+
+
+# Press the green button in the gutter to run the script.
+
+if __name__ == "__main__":
+    print_name('Reddy', '111221007')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
